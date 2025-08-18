59b93ca2c4fbb72a56968baf014b966cd0690a25
Yan Vashchanka <yan.vashchenko@gmail.com>
2025-08-19 01:49:08 +0300

    fix: second number iteration

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -157,8 +157,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -178,8 +178,9 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const semiRes = num + (num % 10 ** pow);
+  return semiRes - (semiRes % 10 ** pow);
 }
 
 /**
@@ -199,8 +200,15 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let res = true;
+  for (let i = 2; i <= Math.round(Math.sqrt(n)); i += 1) {
+    if (n % i === 0) {
+      res = false;
+      break;
+    }
+  }
+  return res;
 }
 
 /**
@@ -218,8 +226,12 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  let res = Number(value);
+  if (String(res) !== String(value)) {
+    res = def;
+  }
+  return res;
 }
 
 module.exports = {
